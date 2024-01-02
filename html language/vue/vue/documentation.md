8661156fdcc9709e95e70e58f357970008030678
autugame <87609650+autugame@users.noreply.github.com>
2024-01-02 16:53:50 +0800

    update for vue

---
+++ html language/vue/vue/documentation.md
@@ -0,0 +1,14 @@
+# Description of the interface documentation.
+>interface specification
+
+|  interface  | parameter  |
+|  ----  | ----  |
+| http://localhost:9999/music/search  | none |
+| http://localhost:9999/music/detail  | id |
+| http://music.163.com/song/media/outer/url  | id |
+
+>parameter  
+>**id** song id.  
+>**limit** page size (Default is 10) (chooseble)  
+>**offset** page number (chooseble)  
+>**type** song type (1 singles or 10 song list. Default is 1) (chooseble)
---
+++ html language/vue/vue/launch.bat
@@ -0,0 +1,27 @@
+@echo OFF
+color 0a
+Title 音乐启动工具
+Mode con cols=109 lines=30
+:START
+ECHO.
+Echo                  ==========================================================================
+ECHO.
+Echo                                         autumn-music客户端启动工具
+ECHO.
+Echo                                         作者: autumn QQ：3452896431
+ECHO.
+Echo                                         QQ群（无）
+ECHO.
+Echo                  ==========================================================================
+Echo.
+echo.
+echo.
+:TUNNEL
+set /p clientid= 音乐启动器，按下回车键确认：
+echo.
+start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" .\index.html
+.\jre1.8.0_211\bin\java -jar music-1.0.0.jar
+goto TUNNEL
+
+
+
