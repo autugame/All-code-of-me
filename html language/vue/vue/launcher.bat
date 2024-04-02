b60184e960ad078628a8b41aac79ce5d8f634e97
autugame <87609650+autugame@users.noreply.github.com>
2024-04-02 21:35:10 +0800

    update for words

--- html language/vue/vue/launcher.bat
+++ html language/vue/vue/launcher.bat
@@ -1,23 +1,23 @@
 @echo OFF
 color 0a
-Title 音乐启动工具
+Title music launcher
 Mode con cols=109 lines=30
 :START
 ECHO.
 Echo                  ==========================================================================
 ECHO.
-Echo                                         autumn-music客户端启动工具
+Echo                                         autumn-music launcher tool
 ECHO.
-Echo                                         作者: autumn QQ：3452896431
+Echo                                         The author: autumn qq number 3452896431
 ECHO.
-Echo                                         QQ群（无）
+Echo                                         QQ group(no)
 ECHO.
 Echo                  ==========================================================================
 Echo.
 echo.
 echo.
 :TUNNEL
-set /p clientid= 音乐启动器，按下回车键确认：
+set /p clientid= music launcher, press enter to continue:
 echo.
 start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" .\indexForVue3.html
 .\jre1.8.0_211\bin\java -jar music-1.0.0.jar
