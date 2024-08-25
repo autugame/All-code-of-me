1d8a05b324fb49f296726c0a60843f43cd68fdaf
autugame <87609650+autugame@users.noreply.github.com>
2024-08-26 00:24:29 +0800

    update for eveything

---
+++ html language/Music Launcher/launcher.bat
@@ -0,0 +1,27 @@
+@echo OFF
+color 0a
+Title music launcher
+Mode con cols=109 lines=30
+:START
+ECHO.
+Echo                  ==========================================================================
+ECHO.
+Echo                                         autumn-music launcher tool
+ECHO.
+Echo                                         The author: autumn qq number 3452896431
+ECHO.
+Echo                                         QQ group(none)
+ECHO.
+Echo                  ==========================================================================
+Echo.
+echo.
+echo.
+:TUNNEL
+set /p clientid= music launcher, press enter to continue:
+echo.
+start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" .\indexForVue3.html
+.\jre1.8.0_211\bin\java -jar music-1.0.0.jar
+goto TUNNEL
+
+
+
