3c88a27c357e7a4cec2c4f72e30a1a439ec9ede5
autugame <87609650+autugame@users.noreply.github.com>
2025-01-27 16:42:59 +0800

    update

---
+++ c++ language/All_C++_Projects/C++/03_variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    cout << "a = " << a << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ c++ language/All_C++_Projects/C++/04_Constant.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+//1. #define
+//2. const
+
+#define DAY 7
+
+int main()
+{
+    const int MONTH = 12;
+
+    cout << "A week has = " << DAY << "days" << endl;
+    cout << "A year has = " << MONTH << "months" << endl;
+
+    system("pause");
+    return 0;
+}
