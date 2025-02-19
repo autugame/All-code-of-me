ac37b211718e74193ef9a4e7acd7bd474c55f48d
autugame <87609650+autugame@users.noreply.github.com>
2025-02-19 16:19:12 +0800

    update for watch dog

---
+++ c language/Embedded/Hal/22-WWDG/Core/Src/wwdg.c
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    wwdg.c
+  * @brief   This file provides code for the configuration
+  *          of the WWDG instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "wwdg.h"
+
+/* USER CODE BEGIN 0 */
+#include "main.h"
+#include "gpio.h"
+/* USER CODE END 0 */
+
+WWDG_HandleTypeDef hwwdg;
+
+/* WWDG init function */
+void MX_WWDG_Init(void)
+{
+
+  /* USER CODE BEGIN WWDG_Init 0 */
+
+  /* USER CODE END WWDG_Init 0 */
+
+  /* USER CODE BEGIN WWDG_Init 1 */
+
+  /* USER CODE END WWDG_Init 1 */
+  hwwdg.Instance = WWDG;
+  hwwdg.Init.Prescaler = WWDG_PRESCALER_8;
+  hwwdg.Init.Window = 95;
+  hwwdg.Init.Counter = 118;
+  hwwdg.Init.EWIMode = WWDG_EWI_ENABLE;
+  if (HAL_WWDG_Init(&hwwdg) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN WWDG_Init 2 */
+
+  /* USER CODE END WWDG_Init 2 */
+
+}
+
+void HAL_WWDG_MspInit(WWDG_HandleTypeDef* wwdgHandle)
+{
+
+  if(wwdgHandle->Instance==WWDG)
+  {
+  /* USER CODE BEGIN WWDG_MspInit 0 */
+
+  /* USER CODE END WWDG_MspInit 0 */
+    /* WWDG clock enable */
+    __HAL_RCC_WWDG_CLK_ENABLE();
+
+    /* WWDG interrupt Init */
+    HAL_NVIC_SetPriority(WWDG_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(WWDG_IRQn);
+  /* USER CODE BEGIN WWDG_MspInit 1 */
+
+  /* USER CODE END WWDG_MspInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+void HAL_WWDG_EarlyWakeupCallback(WWDG_HandleTypeDef *hwwdg)
+{
+	static uint32_t WWDG_Count = 0;
+	WWDG_Count++;
+	if (Key_Value == GPIO_PIN_5)
+	{
+		Key_Value = 0;
+		WWDG_Count = 0;
+	}
+	if (WWDG_Count < 40)
+	{
+		HAL_WWDG_Refresh(hwwdg);
+	}
+}
+/* USER CODE END 1 */
